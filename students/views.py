cc7e3577846adb8c13d889ac36bdba9957819685
Poojitha <pooji.0611@gmail.com>
2025-12-16 17:27:56 +0530

    Added Student Update operation and improve delete URL handling

--- students/views.py
+++ students/views.py
@@ -18,6 +18,20 @@ def student_list(request):
     students = Student.objects.all()
     return render(request, 'student_list.html', {'students' : students})
 
+def update_student(request, id):
+    student = Student.objects.get(id=id)
+
+    if request.method == "POST":
+        student.name = request.POST['name']
+        student.email = request.POST['email']
+        student.age = request.POST['age']
+        student.course = request.POST['course']
+        
+        student.save()
+        return redirect('student_list')
+    
+    return render(request, 'update_student.html', {'student':student})
+
 def delete_student(request, id):
     student = Student.objects.get(id=id)
     student.delete()
